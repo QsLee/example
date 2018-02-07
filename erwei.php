63fa9644466dbd24859aaa392b3c9d262f9cb8ee
QsLee <34386383+QsLee@users.noreply.github.com>
2018-02-07 14:48:02 +0800

    Update erwei.php

--- erwei.php
+++ erwei.php
@@ -1,6 +1,6 @@
 <?php
 include "phpqrcode/phpqrcode.php";
-$value="http://172.20.10.2/li/index.html";
+$value="http://qslee.github.io/example/index.html";
 $errorCorrectionLevel = "L";
 $matrixPointSize = "15";
 QRcode::png($value,false,$errorCorrectionLevel,$matrixPointSize);
